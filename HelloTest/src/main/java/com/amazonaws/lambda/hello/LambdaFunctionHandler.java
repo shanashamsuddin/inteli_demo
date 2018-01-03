8bd7939af141f555e0b8a70dbe2ab0046abbbbee
sam <sverghese@intelitrial.com>
2018-01-03 16:20:02 +0530

    Sample code for Aws lambda

---
+++ HelloTest/src/main/java/com/amazonaws/lambda/hello/LambdaFunctionHandler.java
@@ -0,0 +1,16 @@
+package com.amazonaws.lambda.hello;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaFunctionHandler implements RequestHandler<String, String> {
+
+    @Override
+    public String handleRequest(String input, Context context) {
+        context.getLogger().log("Input: " + input);
+
+        // TODO: implement your handler
+        return input;
+    }
+
+}
